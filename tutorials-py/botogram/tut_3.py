c24173f0922297c46c7e83bb7b648aa9091fcd6c
Abhijit Roy <dalvath3700@gmail.com>
2021-04-02 22:29:32 +0530

    tut 3 updated

--- tutorials-py/botogram/tut_3.py
+++ tutorials-py/botogram/tut_3.py
@@ -1,16 +1,21 @@
 """
     Command arguments parse
-    ************************
+    ***********************
     Demo
     ====
 
     user: 
-        /withdrawmemo tipuser11111 1.0000 EOS pay_bill
+        /tip tipuser11112 0.1000 EOS restaurant_tip_for_excellent_service
     bot: 
-        arg0: tipuser11111
-        arg1: 1.0000
+        arg0: tipuser11112
+        arg1: 0.1000
         arg2: EOS
-        arg3: pay_bill
+        arg3: restaurant_tip_for_excellent_service
+    user: 
+        /tip
+    bot: 
+        Please enter tip request (with memo) in this format: /tip RECEIVER_ACCOUNT AMOUNT SYMBOL MEMO 
+        (e.g. /tip tipuser11112 0.1000 EOS restaurant_tip_for_excellent_service)
 """
 
 import botogram
@@ -18,21 +23,23 @@ from input import *
 
 bot = botogram.create(API_key)    
 
-@bot.command("withdrawmemo")
-def withdrawmemo_command(chat, message, args):
+@bot.command("tip")
+def tip_command(chat, message, args):
     """
-        Withdraw your EOSIO token from this bot to your EOSIO account with a memo
+        Tip your EOSIO token from this bot to a user (with telegram id) along with a memo
         NOTE: Please don't use 'space' in between words in `memo`. Instead, use 'underscore', 'hyphen', etc.
         
         Demo:
         =====
         User: 
-            /withdrawmemo tipuser11111 1.0000 EOS pay_bill           
+            /tip tipuser11112 0.1000 EOS restaurant_tip_for_excellent_service
+        Bot:
+            DONE!
     """
     if len(args) == 4:
-        chat.send(f"arg0: {args[0]}\narg1: {args[1]}\narg2: {args[2]}\narg3: {args[3]}")        # for testing
+        chat.send(f"arg0: {args[0]}\narg1: {args[1]}\narg2: {args[2]}\narg3: {args[3]}", syntax="plain")        # for testing
     else:
-        chat.send("Please enter withdrawal request (with memo) in this format: /withdrawmemo ACCOUNT AMOUNT SYMBOL MEMO \n(e.g. /withdrawmemo tipuser11111 1.0000 EOS pay_bill)")
+        chat.send("Please enter tip request (with memo) in this format: /tip RECEIVER_ACCOUNT AMOUNT SYMBOL MEMO \n(e.g. /tip tipuser11112 0.1000 EOS restaurant_tip_for_excellent_service)", syntax="plain")
 
 if __name__ == "__main__":
     bot.run()
