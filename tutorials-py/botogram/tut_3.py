4b7ee547c31643e02155afad5dc5002a31ce3180
Abhijit Roy <dalvath3700@gmail.com>
2021-04-02 12:02:47 +0530

    updated tut_@

--- tutorials-py/botogram/tut_3.py
+++ tutorials-py/botogram/tut_3.py
@@ -5,11 +5,12 @@
     ====
 
     user: 
-        /withdraw sdf 1.0000 EOS
+        /withdrawmemo tipuser11111 1.0000 EOS pay_bill
     bot: 
-        arg0: sdf
+        arg0: tipuser11111
         arg1: 1.0000
         arg2: EOS
+        arg3: pay_bill
 """
 
 import botogram
@@ -17,10 +18,21 @@ from input import *
 
 bot = botogram.create(API_key)    
 
-@bot.command("withdraw")
-def withdraw_command(chat, message, args):
-    """Withdraw your EOSIO token from this bot to your EOSIO account"""
-    chat.send(f"arg0: {args[0]}\narg1: {args[1]}\narg2: {args[2]}\n")
+@bot.command("withdrawmemo")
+def withdrawmemo_command(chat, message, args):
+    """
+        Withdraw your EOSIO token from this bot to your EOSIO account with a memo
+        NOTE: Please don't use 'space' in between words in `memo`. Instead, use 'underscore', 'hyphen', etc.
+        
+        Demo:
+        =====
+        User: 
+            /withdrawmemo tipuser11111 1.0000 EOS pay_bill           
+    """
+    if len(args) == 4:
+        chat.send(f"arg0: {args[0]}\narg1: {args[1]}\narg2: {args[2]}\narg3: {args[3]}")        # for testing
+    else:
+        chat.send("Please enter withdrawal request (with memo) in this format: /withdrawmemo ACCOUNT AMOUNT SYMBOL MEMO \n(e.g. /withdrawmemo tipuser11111 1.0000 EOS pay_bill)")
 
 if __name__ == "__main__":
     bot.run()
